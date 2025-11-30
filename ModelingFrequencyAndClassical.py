1312014fd9a8e8300c48cd4e86642143b8927cf5
D-ASA-D <vladruadov@gmail.com>
2025-12-01 00:05:08 +0300

    ModelingFrequencyAndClassical

---
+++ ModelingFrequencyAndClassical.py
@@ -0,0 +1,34 @@
+from random import sample
+import plotly.graph_objs as go
+import numpy as np
+from scipy.special import comb
+def random_combination(redBalls, blueBalls, targetRed, targetBlue):
+    urna = ['R'] * redBalls + ['B'] * blueBalls
+    return sample(urna, targetRed + targetBlue)
+
+redBalls = int(input("Введите количество красных шаров:"))
+blueBalls = int(input("Введите количество синих шаров:"))
+targetRed = int(input("Введите количество красных вынимаемых шаров:"))
+targetBlue = int(input("Введите количество синих вынимаемых шаров:"))
+
+N_values = np.array(range(10, 10001, 50))
+P_A_values = []
+for N in N_values:
+    N_A = 0
+    for _ in range(N):
+        Comb = random_combination(redBalls, blueBalls, targetRed, targetBlue)
+        if Comb.count('R') == targetRed:
+            N_A += 1
+    P_A_values.append(N_A / N)
+pTeor = (comb(redBalls, targetRed) * comb(blueBalls, targetBlue))/comb(redBalls+blueBalls, targetRed+targetBlue)
+print(f"Теоретический расчет вероятности: {pTeor}")
+print(f"Частотный расчет вероятности: {round(P_A_values[-1], 4)}")
+P = [pTeor] * len(N_values)
+fig = go.Figure(layout=dict(width=600, height=300))
+fig.update_layout(margin=dict(l=0, t=0, b=0))
+fig.add_trace(go.Scatter(x=N_values, y=P_A_values, name='Частота'))
+fig.add_trace(go.Scatter(x=N_values, y=P,
+name='Вероятность', line_dash='dash'))
+fig.update_xaxes(title_text='N')
+fig.update_yaxes(title_text='P(A)')
+fig.show();
